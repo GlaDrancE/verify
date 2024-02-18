f7853f5c89b8ed6478f0e23f2da25da09696df85
GlaDrancE <84795357+GlaDrancE@users.noreply.github.com>
2024-02-18 20:44:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Connect to MongoDB database
